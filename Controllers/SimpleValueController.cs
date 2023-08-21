a47f81434a63bf73d56448a5d1e3e4914ebb5d8b
n4tadecoco <ktc2243030002@edu.kyoto-tech.ac.jp>
2023-08-21 16:19:55 +0900

    最後にオリジナルを追加してますがazureの無料分を使い切ってしまったので動くかわからないです。
    最後にオリジナルを追加してますがazureの無料分を使い切ってしまったので動くかわからないです。

--- Controllers/SimpleValueController.cs
+++ Controllers/SimpleValueController.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 using OrleansWebAPI7AppDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure;
+using Azure.AI.OpenAI;
+using DotNetEnv;
 
 namespace OrleansWebAPI7AppDemo.Controllers
 {
@@ -11,14 +17,20 @@ namespace OrleansWebAPI7AppDemo.Controllers
 
         public SimpleValueController()
         {
+            Env.Load();
         }
 
         [HttpGet()]
         public String TestValue()
         {
-            return "あいうえお";
+            return "jaja";
         }
 
+        [HttpGet()]
+        public String Hi()
+        {
+            return "jojo";
+        }
 
         [HttpGet()]
         public IEnumerable<string> TestArray()
@@ -46,6 +58,27 @@ namespace OrleansWebAPI7AppDemo.Controllers
         {
             animal.Age = 20;
             return animal;
+        }
+        [HttpGet("{question}")]
+        public async Task<IActionResult> AnswerQuestion(string question)
+        {
+            try
+            {
+                var openAiEndpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+                var openAiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_KEY");
+
+                var openAiClient = new OpenAIClient(new Uri(openAiEndpoint), new AzureKeyCredential(openAiKey));
+
+                var prompt = question;
+                var completionsResponse = await openAiClient.GetCompletionsAsync("text-davinci-003", prompt);
+                var completion = completionsResponse.Value.Choices[0].Text;
+
+                return Ok(new { Question = question, Answer = completion });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
         }
     }
 }
